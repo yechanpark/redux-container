a36d0a97b9f6de4cb1532679f77a0f5c0913ad15
yechanpark <qkrdpcks0419@naver.com>
2020-05-08 18:15:51 +0900

    first commit

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,11 @@
+/* Action 종류들을 선언
+   앞에 export를 붙이면 나중에 이것들을 불러올 때,
+   import * as types from './ActionTypes';와 같은 처리가 가능하다.
+ */
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
+
+export const CREATE = 'CREATE';
+export const REMOVE = 'REMOVE';
